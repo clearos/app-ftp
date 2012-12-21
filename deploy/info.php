6cb17605365170bdb47e928d68d7f80fc88e59a2
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-12-21 22:09:04 +0000

    - Added to 6.4.0 Beta 1  build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'ftp';
-$app['version'] = '1.4.9';
+$app['version'] = '1.4.10';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
