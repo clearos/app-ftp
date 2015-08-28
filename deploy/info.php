3ee3622e16209e0ba3241824f01db8a07970da64
pcbaldwin <pbaldwin@clearfoundation.com>
2015-08-28 16:43:45 -0400

    Added uninstall for Flexshare hooks [tracker #2289]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'ftp';
-$app['version'] = '2.1.6';
+$app['version'] = '2.1.18';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-ftp.spec
+++ packaging/app-ftp.spec
@@ -1,7 +1,7 @@
 
 Name: app-ftp
 Epoch: 1
-Version: 2.1.6
+Version: 2.1.18
 Release: 1%{dist}
 Summary: FTP Server
 License: GPLv3
