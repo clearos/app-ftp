af95b73ddb80e8062f0a606f780b9d303571049a
pcbaldwin <peter@egloo.ca>
2016-09-20 16:14:34 -0400

    Added warning in fail2ban config [tracker #10741]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'ftp';
-$app['version'] = '2.2.2';
+$app['version'] = '2.3.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-ftp.spec
+++ packaging/app-ftp.spec
@@ -1,7 +1,7 @@
 
 Name: app-ftp
 Epoch: 1
-Version: 2.2.2
+Version: 2.3.0
 Release: 1%{dist}
 Summary: FTP Server
 License: GPLv3
