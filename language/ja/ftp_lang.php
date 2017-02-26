168d3d22d97b0ad61ed692f64102bfbefd57b5f8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:51:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTPサーバーは、標準のFTPおよびFTPセキュアなプロトコルを使用して、ファイル管理とストレージを提供します';
+$lang['ftp_app_name'] = 'FTPサーバー';
+$lang['ftp_failed_logins'] = 'FTPログイン失敗';
+$lang['ftp_flexshare_port'] = 'FlexShareのポート';
+$lang['ftp_home_share_port'] = 'ホームシェアリングポート';
+$lang['ftp_max_instances'] = '最大インスタンス';
+$lang['ftp_maximum_instances_invalid'] = '最大インスタンス数が無効です。';
+$lang['ftp_port_reserved_for_flexshare'] = 'FlexShareのためのポートに予約されています。';
+$lang['ftp_server_name'] = 'サーバーの名前';
+$lang['ftp_server_name_invalid'] = 'サーバー名が無効です。';
