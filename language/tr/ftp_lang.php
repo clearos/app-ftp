ea253e88329962f2a704ab279a3a72a457f69245
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:57:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP Server standart FTP ve FTP Güvenli protokollerini kullanarak dosya yönetimi ve depolama sağlar';
+$lang['ftp_app_name'] = 'FTP Server';
+$lang['ftp_failed_logins'] = 'FTP oturumu açma başarısız';
+$lang['ftp_flexshare_port'] = 'Flexshare Limanı';
+$lang['ftp_home_share_port'] = 'Ev Hisse Liman';
+$lang['ftp_max_instances'] = 'maksimum örnekleri';
+$lang['ftp_maximum_instances_invalid'] = 'Maksimum örnekleri geçersiz.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Flexshare Port Saklıdır.';
+$lang['ftp_server_name'] = 'Sunucu adı';
+$lang['ftp_server_name_invalid'] = 'Sunucu adı geçersiz.';
