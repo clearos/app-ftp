e9e27fff46ee952f243a4169d39aeb37c7cd1ad0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:56:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'Server FTP waxay bixisaa maamulka file iyo kaydinta la isticmaalayo FTP iyo FTP hab-Secure caadiga ah';
+$lang['ftp_app_name'] = 'Server FTP';
+$lang['ftp_failed_logins'] = 'FTP fashilmay magacyo';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Home Share Port';
+$lang['ftp_max_instances'] = 'Xaaladaha ugu badan';
+$lang['ftp_maximum_instances_invalid'] = 'Mararka ugu badan waa baadil.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port Reserved for Flexshare.';
+$lang['ftp_server_name'] = 'server Name';
+$lang['ftp_server_name_invalid'] = 'Magaca Server waa baadil.';
