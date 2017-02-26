b6317ca66ce38c3b11cc5d93fc833044d36be836
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:57:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'Ang FTP Server ay nagbibigay ng pamamahala ng file at imbakan gamit ang karaniwang FTP at FTP Secure protocol';
+$lang['ftp_app_name'] = 'FTP Server';
+$lang['ftp_failed_logins'] = 'FTP Nabigong Pag-login';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Home Share Port';
+$lang['ftp_max_instances'] = 'Maximum Pagkakataon';
+$lang['ftp_maximum_instances_invalid'] = 'Maximum pagkakataon ay hindi wasto.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port ay Nakareserba para Flexshare.';
+$lang['ftp_server_name'] = 'Pangalan ng server';
+$lang['ftp_server_name_invalid'] = 'Pangalan ng server ay hindi wasto.';
