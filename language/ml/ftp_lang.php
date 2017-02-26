e5f3ce87363a5fbc66361ba3ca8b7d925fe6cafd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:53:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP സർവർ സാധാരണ എഫ്ടിപി, എഫ്ടിപി സുരക്ഷിത പ്രോട്ടോക്കോളുകൾ ഉപയോഗിച്ച് ഫയൽ മാനേജ്മെന്റ്, സ്റ്റോറേജ് ലഭ്യമാക്കുന്നു';
+$lang['ftp_app_name'] = 'FTP സെർവർ';
+$lang['ftp_failed_logins'] = 'എഫ്ടിപി പരാജയപ്പെട്ടു പ്രവേശിക്കലുകൾ';
+$lang['ftp_flexshare_port'] = 'Flexshare പോര്ട്';
+$lang['ftp_home_share_port'] = 'ഹോം ഷെയർ പോര്ട്';
+$lang['ftp_max_instances'] = 'പരമാവധി ഉദാഹരണങ്ങൾ';
+$lang['ftp_maximum_instances_invalid'] = 'പരമാവധി ഇൻസ്റ്റൻസുകൾ അസാധുവാണ്.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Flexshare പോർട്ട് reserved.';
+$lang['ftp_server_name'] = 'സെർവർ പേര്';
+$lang['ftp_server_name_invalid'] = 'സെർവർ പേര് അസാധുവാണ്.';
