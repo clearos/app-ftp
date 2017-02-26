cc33a26229e0ee3c31c7a124944682c45c9ac5e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:47:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'The FTP Server ապահովում ֆայլի կառավարման եւ պահպանման, օգտագործելով ստանդարտ FTP եւ FTP Անվտանգ արձանագրությունները';
+$lang['ftp_app_name'] = 'FTP Server';
+$lang['ftp_failed_logins'] = 'FTP ձախողված';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Home Share Port';
+$lang['ftp_max_instances'] = 'առավելագույն դեպքեր';
+$lang['ftp_maximum_instances_invalid'] = 'Առավելագույն դեպքեր է անվավեր:';
+$lang['ftp_port_reserved_for_flexshare'] = 'Պորտ պաշտպանված են Flexshare.';
+$lang['ftp_server_name'] = 'Server Անունը';
+$lang['ftp_server_name_invalid'] = 'Սերվերի անունը անվավեր է:';
