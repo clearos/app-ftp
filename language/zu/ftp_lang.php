d3e5810f37232668ca5b311b1f9d8fc71db5fb40
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:59:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'I Iseva ye-FTP inikeza ukuphathwa ifayela futhi isitoreji usebenzisa standard FTP futhi FTP amaphrothokholi Secure';
+$lang['ftp_app_name'] = 'FTP Server';
+$lang['ftp_failed_logins'] = 'FTP Yehlulekile ungene';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Home Yabelana Port';
+$lang['ftp_max_instances'] = 'Izimo Maximum';
+$lang['ftp_maximum_instances_invalid'] = 'Kukhokonke Maximum ayivumelekile.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port Agodliwe for Flexshare.';
+$lang['ftp_server_name'] = 'Igama leseva';
+$lang['ftp_server_name_invalid'] = 'Igama leseva alivumelekile.';
