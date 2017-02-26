3ba06f4a8647c491866dcf63742421afb0e493ad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:58:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'די FTP סערווירער גיט טעקע פאַרוואַלטונג און סטאָרידזש ניצן די סטאַנדאַרט FTP און FTP זיכער פּראָטאָקאָלס';
+$lang['ftp_app_name'] = 'FTP סערווירער';
+$lang['ftp_failed_logins'] = 'FTP אַנדערש לאָגינס';
+$lang['ftp_flexshare_port'] = 'פלעקסשאַרע פּאָרט';
+$lang['ftp_home_share_port'] = 'היים שער פּאָרט';
+$lang['ftp_max_instances'] = 'מאַקסימום ינסטאַנסעס';
+$lang['ftp_maximum_instances_invalid'] = 'מאַקסימום ינסטאַנסיז איז פאַרקריפּלט.';
+$lang['ftp_port_reserved_for_flexshare'] = 'פּאָרט רעזערווירט פֿאַר פלעקסשאַרע.';
+$lang['ftp_server_name'] = 'סערווירער נאָמען';
+$lang['ftp_server_name_invalid'] = 'סערווירער נאָמען איז פאַרקריפּלט.';
