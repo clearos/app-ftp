73a1861522617fd7c11e465050b69d97aac59adf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:47:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP zerbitzariak fitxategi kudeaketa eta biltegiratze estandarra FTP FTP eta segurua protokoloak erabiliz eskaintzen';
+$lang['ftp_app_name'] = 'FTP Zerbitzaria';
+$lang['ftp_failed_logins'] = 'FTP huts egindako sarrerak';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Hasiera Share Port';
+$lang['ftp_max_instances'] = 'gehienezko Instantziak';
+$lang['ftp_maximum_instances_invalid'] = 'Gehienezko instantzia baliogabea da.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port erreserbatuta Flexshare da.';
+$lang['ftp_server_name'] = 'Server Name';
+$lang['ftp_server_name_invalid'] = 'Zerbitzari-izena baliogabea da.';
