6a91f30e1f7ddde3f403c2dc9228a45251806a3a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:50:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'The FTP Server samar file management da kuma ajiya ta amfani da daidaitattun FTP kuma FTP Secure ladabi';
+$lang['ftp_app_name'] = 'FTP Server';
+$lang['ftp_failed_logins'] = 'FTP nasarar logins';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Home Share Port';
+$lang['ftp_max_instances'] = 'Maximum lokutta';
+$lang['ftp_maximum_instances_invalid'] = 'Maximum lokutta ba daidai ba ne.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port tanada Flexshare.';
+$lang['ftp_server_name'] = 'Server Name';
+$lang['ftp_server_name_invalid'] = 'Server name shi ne daidai ba.';
