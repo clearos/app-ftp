f9ab45b030a20093473fed1f13279596224a5417
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:50:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'Az FTP Server fájlkezelés és tárolás a szabványos FTP és FTP biztonságos protokollok';
+$lang['ftp_app_name'] = 'FTP szerver';
+$lang['ftp_failed_logins'] = 'FTP sikertelen bejelentkezés';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Főoldal Share Port';
+$lang['ftp_max_instances'] = 'maximális példányok';
+$lang['ftp_maximum_instances_invalid'] = 'Maximális esetben érvénytelen.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port fenntartva Flexshare.';
+$lang['ftp_server_name'] = 'Szerver név';
+$lang['ftp_server_name_invalid'] = 'Szerver neve érvénytelen.';
