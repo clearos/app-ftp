a8b7bb56acbcbdc900b62f90d5037326509f1481
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:52:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP serveris suteikia failų valdymo ir saugojimo naudojant standartines FTP ir FTP saugių protokolų';
+$lang['ftp_app_name'] = 'FTP serveris';
+$lang['ftp_failed_logins'] = 'FTP nesėkmingų prisijungimų';
+$lang['ftp_flexshare_port'] = 'Flexshare uostas';
+$lang['ftp_home_share_port'] = 'Namai Dalintis uostas';
+$lang['ftp_max_instances'] = 'Didžiausias egzempliorių';
+$lang['ftp_maximum_instances_invalid'] = 'Didžiausios atvejų yra neteisinga.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Uosto Rezervuotas Flexshare.';
+$lang['ftp_server_name'] = 'Serverio pavadinimas';
+$lang['ftp_server_name_invalid'] = 'Serverio pavadinimas yra neteisingas.';
