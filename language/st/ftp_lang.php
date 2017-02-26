9bf58ab3cc84448fb96d19803ec3189af5f9b726
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:56:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'The Server FTP fana ka taolo ya faele le poloko sebelisa tekanyetso FTP le FTP Sireletseha diprothokole';
+$lang['ftp_app_name'] = 'Server FTP';
+$lang['ftp_failed_logins'] = 'FTP hlōleha Logins';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Home Share Port';
+$lang['ftp_max_instances'] = 'palo e kahodimodimo Maemong';
+$lang['ftp_maximum_instances_invalid'] = 'Palo e kahodimodimo Mabakeng ha o sebetse.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port Reserved bakeng Flexshare.';
+$lang['ftp_server_name'] = 'Name Server';
+$lang['ftp_server_name_invalid'] = 'lebitso Server ha o sebetse.';
