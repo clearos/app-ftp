ac0be5c80c95f8cca9205a0ffcd2de52573da1c1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:54:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP Server gir filbehandling og lagring ved hjelp av standard FTP og FTP sikre protokoller';
+$lang['ftp_app_name'] = 'FTP-server';
+$lang['ftp_failed_logins'] = 'FTP Kunne pålogginger';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Hjemmedeling Port';
+$lang['ftp_max_instances'] = 'Maksimalt antall forekomster';
+$lang['ftp_maximum_instances_invalid'] = 'Maksimum tilfeller er ugyldig.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port Reservert for Flexshare.';
+$lang['ftp_server_name'] = 'Server navn';
+$lang['ftp_server_name_invalid'] = 'Server navn er ugyldig.';
