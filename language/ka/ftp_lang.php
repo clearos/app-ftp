1e1fb09eaf2cb39eb0fe7d5d997d31ff68a24f10
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:51:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP სერვერი უზრუნველყოფს ფაილის მართვა და შენახვის გამოყენებით სტანდარტული FTP და FTP უსაფრთხო ოქმები';
+$lang['ftp_app_name'] = 'FTP სერვერი';
+$lang['ftp_failed_logins'] = 'FTP ვერ Logins';
+$lang['ftp_flexshare_port'] = 'Flexshare პორტი';
+$lang['ftp_home_share_port'] = 'მთავარი Share პორტი';
+$lang['ftp_max_instances'] = 'მაქსიმალური შემთხვევები';
+$lang['ftp_maximum_instances_invalid'] = 'მაქსიმალური შემთხვევები არასწორია.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port დაცულია Flexshare.';
+$lang['ftp_server_name'] = 'სერვერის სახელი';
+$lang['ftp_server_name_invalid'] = 'სერვერის სახელი არასწორია.';
