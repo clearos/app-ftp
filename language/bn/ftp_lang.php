3f60c6d576f05159d8fc5c36c22dded1fb79e31c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:48:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP সার্ভারের ফাইল ব্যবস্থাপনা এবং স্টোরেজ মান FTP ও এফটিপি সিকিউর প্রোটোকল ব্যবহার করে উপলব্ধ';
+$lang['ftp_app_name'] = 'FTP সার্ভারের';
+$lang['ftp_failed_logins'] = 'এফটিপি লগইন ব্যর্থ';
+$lang['ftp_flexshare_port'] = 'Flexshare পোর্ট';
+$lang['ftp_home_share_port'] = 'বাড়ি পোর্ট';
+$lang['ftp_max_instances'] = 'সর্বোচ্চ দৃষ্টান্ত';
+$lang['ftp_maximum_instances_invalid'] = 'সর্বোচ্চ দৃষ্টান্ত অবৈধ.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Flexshare জন্য পোর্ট সংরক্ষিত.';
+$lang['ftp_server_name'] = 'সার্ভার নাম';
+$lang['ftp_server_name_invalid'] = 'সার্ভার নাম অবৈধ.';
