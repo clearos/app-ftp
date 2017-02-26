793a287bd65e1f05768879bcaf17487465c4f864
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:58:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP Server cung cấp quản lý tập tin và lưu trữ bằng cách sử dụng tiêu chuẩn FTP và FTP giao thức bảo mật';
+$lang['ftp_app_name'] = 'FTP server';
+$lang['ftp_failed_logins'] = 'FTP Không Logins';
+$lang['ftp_flexshare_port'] = 'Cảng Flexshare';
+$lang['ftp_home_share_port'] = 'Trang chủ Chia sẻ Cảng';
+$lang['ftp_max_instances'] = 'Trường hợp tối đa';
+$lang['ftp_maximum_instances_invalid'] = 'trường hợp tối đa là không hợp lệ.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Cảng Reserved cho Flexshare.';
+$lang['ftp_server_name'] = 'Tên máy chủ';
+$lang['ftp_server_name_invalid'] = 'tên máy chủ không hợp lệ.';
