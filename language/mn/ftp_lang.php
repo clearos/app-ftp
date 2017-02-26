69dd78c129182148cfc9442fb498ca879a35563d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:53:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP сервер стандарт FTP болон FTP Secure протоколыг ашиглан файл менежмент, хадгалах өгдөг';
+$lang['ftp_app_name'] = 'FTP сервер';
+$lang['ftp_failed_logins'] = 'FTP биш нэвтрэлтүүдийг хааж чадсангүй';
+$lang['ftp_flexshare_port'] = 'Flexshare Порт';
+$lang['ftp_home_share_port'] = 'Нүүр хуудас Share Порт';
+$lang['ftp_max_instances'] = 'Хамгийн их тохиолдол';
+$lang['ftp_maximum_instances_invalid'] = 'Хамгийн их тохиолдол хүчин төгөлдөр бус байна.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Flexshare нь Порт хуулиар хамгаалагдсан.';
+$lang['ftp_server_name'] = 'Server нэр';
+$lang['ftp_server_name_invalid'] = 'Серверийн нэр хүчингүй байна.';
