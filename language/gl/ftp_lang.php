dca45571b0bba0efc6ee0bafbb2a82e2f0f352a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:49:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'O servidor FTP fornece xestión e almacenamento de arquivos usando o estándar FTP e FTP protocolos seguros';
+$lang['ftp_app_name'] = 'servidor FTP';
+$lang['ftp_failed_logins'] = 'FTP Erro logins';
+$lang['ftp_flexshare_port'] = 'FlexShare Porto';
+$lang['ftp_home_share_port'] = 'Inicio Compartir Porto';
+$lang['ftp_max_instances'] = 'máximo de instancias';
+$lang['ftp_maximum_instances_invalid'] = 'instancias máximas non é válido.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Porta reservada para FlexShare.';
+$lang['ftp_server_name'] = 'Name Server';
+$lang['ftp_server_name_invalid'] = 'nome do servidor non é válido.';
