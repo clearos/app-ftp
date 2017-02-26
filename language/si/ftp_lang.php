4c59953a160ad402f48c3aa45e1ab0d6ba7d0bea
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:55:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP සේවාදායකයට සම්මත FTP සහ FTP ආරක්ෂිත ප්රොටෝකෝල ගොනුව භාවිතා කළමනාකරණ හා ගබඩා සපයයි';
+$lang['ftp_app_name'] = 'FTP සේවාදායකය';
+$lang['ftp_failed_logins'] = 'FTP අසාර්ථක උත්සහයන්';
+$lang['ftp_flexshare_port'] = 'Flexshare වරාය';
+$lang['ftp_home_share_port'] = 'මුල් පිටුව හුවමාරු වරාය';
+$lang['ftp_max_instances'] = 'උපරිම අවස්ථා';
+$lang['ftp_maximum_instances_invalid'] = 'උපරිම අවස්ථා වලංගු නැත.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Flexshare සඳහා වරාය ඇවිරිනි.';
+$lang['ftp_server_name'] = 'සේවාදායක නම';
+$lang['ftp_server_name_invalid'] = 'සේවාදායකයේ නම වලංගු නැත.';
