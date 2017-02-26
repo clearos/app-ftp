e51c8029cfad03a0112c62c3e754d5a67481612d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:54:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP ਸਰਵਰ ਫਾਇਲ ਪ੍ਰਬੰਧਨ ਅਤੇ ਸਟੋਰੇਜ਼ ਮਿਆਰੀ FTP ਅਤੇ FTP, ਸੁਰੱਖਿਅਤ ਪਰੋਟੋਕਾਲ ਵਰਤ ਪ੍ਰਦਾਨ ਕਰਦਾ ਹੈ';
+$lang['ftp_app_name'] = 'FTP ਸਰਵਰ';
+$lang['ftp_failed_logins'] = 'FTP, ਲਾਗਇਨ ਵਿੱਚ ਅਸਫਲ';
+$lang['ftp_flexshare_port'] = 'Flexshare ਪੋਰ੍ਟ';
+$lang['ftp_home_share_port'] = 'ਮੁੱਖ ਨਿਯਤ ਕਰੋ ਪੋਰ੍ਟ';
+$lang['ftp_max_instances'] = 'ਵੱਧ ਮੌਕੇ';
+$lang['ftp_maximum_instances_invalid'] = 'ਵੱਧ ਮੌਕੇ ਗਲਤ ਹੈ.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Flexshare ਲਈ ਪੋਰ੍ਟ ਰਾਖਵ ਹਨ.';
+$lang['ftp_server_name'] = 'ਸਰਵਰ ਨਾਮ';
+$lang['ftp_server_name_invalid'] = 'ਸਰਵਰ ਨਾਮ ਗਲਤ ਹੈ.';
