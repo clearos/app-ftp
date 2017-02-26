5ceffea80793979ade952d1119f8856cebec26b8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:47:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'يوفر ملقم FTP إدارة الملفات والتخزين باستخدام بروتوكول نقل الملفات وبروتوكول نقل الملفات البروتوكولات الآمنة القياسية';
+$lang['ftp_app_name'] = 'خادم بروتوكول نقل الملفات';
+$lang['ftp_failed_logins'] = 'فشل بروتوكول نقل الملفات محدودة';
+$lang['ftp_flexshare_port'] = 'ميناء Flexshare';
+$lang['ftp_home_share_port'] = 'الصفحة الرئيسية مشاركة بورت';
+$lang['ftp_max_instances'] = 'حالات القصوى';
+$lang['ftp_maximum_instances_invalid'] = 'الحالات القصوى غير صالحة.';
+$lang['ftp_port_reserved_for_flexshare'] = 'ميناء محفوظة لموقع Flexshare.';
+$lang['ftp_server_name'] = 'اسم الخادم';
+$lang['ftp_server_name_invalid'] = 'اسم الملقم غير صالح.';
