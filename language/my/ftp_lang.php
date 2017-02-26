cedadb2127e0aa8e3a43759ba3a5e5db14747c67
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:48:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'အဆိုပါတယောက်က FTP Server ကိုစံ FTP နှင့် FTP ကိုလုံလုံခြုံခြုံ protocol များကို အသုံးပြု. ဖိုင်စီမံခန့်ခွဲမှုနှင့်သိုလှောင်ပေးစွမ်းသည်';
+$lang['ftp_app_name'] = 'တယောက်က FTP Server ကို';
+$lang['ftp_failed_logins'] = 'FTP ကိုမရခြင်း login';
+$lang['ftp_flexshare_port'] = 'Flexshare ဆိပ်ကမ်း';
+$lang['ftp_home_share_port'] = 'မူလစာမျက်နှာဝေမျှမယ်ဆိပ်ကမ်း';
+$lang['ftp_max_instances'] = 'အများဆုံးဖြစ်စဥ်များ';
+$lang['ftp_maximum_instances_invalid'] = 'အများဆုံးသာဓကမမှန်ကန်ဘူး။';
+$lang['ftp_port_reserved_for_flexshare'] = 'Flexshare များအတွက်ဆိပ်ကမ်းချုပ်ထိန်းထားသည်။';
+$lang['ftp_server_name'] = 'Server ကိုအမည်';
+$lang['ftp_server_name_invalid'] = 'Server ကိုအမည်မမှန်ကန်ပါ။';
