2ae54eb9941442e73778bffcd661e5b7e60bfa55
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:51:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP Server veitir skrá stjórnun og geymslu með því að nota staðlaða FTP og FTP Öruggur samskiptareglum';
+$lang['ftp_app_name'] = 'FTP Server';
+$lang['ftp_failed_logins'] = 'FTP mistókst Innskráningar';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Home Share Port';
+$lang['ftp_max_instances'] = 'Hámark Tilvik';
+$lang['ftp_maximum_instances_invalid'] = 'Hámark dæmi er ógilt.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port Reserved fyrir Flexshare.';
+$lang['ftp_server_name'] = 'Server Name';
+$lang['ftp_server_name_invalid'] = 'Nafn miðlara er ógilt.';
