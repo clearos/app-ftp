61a8a0b539d7ed2e7cdf6702ee0063dd02e0f6ae
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:53:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP сервер обезбедува управување со датотеки и складирање со користење на стандарден FTP и FTP безбедна протоколи';
+$lang['ftp_app_name'] = 'FTP сервер';
+$lang['ftp_failed_logins'] = 'FTP неуспешни обиди за најава';
+$lang['ftp_flexshare_port'] = 'Flexshare пристаниште';
+$lang['ftp_home_share_port'] = 'Насловна Сподели пристаниште';
+$lang['ftp_max_instances'] = 'максимална Случаите';
+$lang['ftp_maximum_instances_invalid'] = 'Максимална случаи не е валиден.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Порт Резервирано за Flexshare.';
+$lang['ftp_server_name'] = 'име на серверот';
+$lang['ftp_server_name_invalid'] = 'име на серверот не е валиден.';
