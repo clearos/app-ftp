6c408623c8ea3c489a930ba984e0123cf888f570
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:56:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'Nu ftp Server nyadiakeun manajemén file jeung neundeun ngagunakeun standar ftp jeung ftp protokol aman';
+$lang['ftp_app_name'] = 'ftp Server';
+$lang['ftp_failed_logins'] = 'Ftp Gagal Logins';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Home Share Port';
+$lang['ftp_max_instances'] = 'instansi maksimum';
+$lang['ftp_maximum_instances_invalid'] = 'instansi nu maksimum téh sah.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port wengkuan keur Flexshare.';
+$lang['ftp_server_name'] = 'server Ngaran';
+$lang['ftp_server_name_invalid'] = 'ngaran Server nyaéta sah.';
