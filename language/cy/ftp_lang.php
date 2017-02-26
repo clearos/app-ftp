79544eae16d1706e9e4a2e938ff7382cf3e472c5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:58:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'Mae&#39;r Gweinyddwr FTP yn darparu rheoli ffeiliau a storio gan ddefnyddio FTP a FTP protocolau Diogel safonol';
+$lang['ftp_app_name'] = 'FTP Gweinyddwr';
+$lang['ftp_failed_logins'] = 'Methwyd FTP mewngofnodi';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Hafan Share Port';
+$lang['ftp_max_instances'] = 'enghreifftiau uchafswm';
+$lang['ftp_maximum_instances_invalid'] = 'achosion Uchafswm yn annilys.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port Reserved i Flexshare.';
+$lang['ftp_server_name'] = 'gweinydd Enw';
+$lang['ftp_server_name_invalid'] = 'enw&#39;r gweinydd yn annilys.';
