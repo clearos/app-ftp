f85ecde998238711a69ed359e09802c937229ba8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:52:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP 서버는 표준 FTP와 FTP 보안 프로토콜을 사용하여 파일 관리 및 스토리지를 제공합니다';
+$lang['ftp_app_name'] = 'FTP 서버';
+$lang['ftp_failed_logins'] = 'FTP는 로그인 실패';
+$lang['ftp_flexshare_port'] = 'FlexShare를 포트';
+$lang['ftp_home_share_port'] = '홈 공유 포트';
+$lang['ftp_max_instances'] = '최대 인스턴스';
+$lang['ftp_maximum_instances_invalid'] = '최대 인스턴스가 잘못되었습니다.';
+$lang['ftp_port_reserved_for_flexshare'] = 'FlexShare를위한 포트 소유.';
+$lang['ftp_server_name'] = '서버 이름';
+$lang['ftp_server_name_invalid'] = '서버 이름이 잘못되었습니다.';
