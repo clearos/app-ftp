0344b5ab4445070c44283b297b56b7955b8039b1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:48:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP服务器提供了使用标准的FTP和FTP安全协议文件管理和存储';
+$lang['ftp_app_name'] = 'FTP服务器';
+$lang['ftp_failed_logins'] = 'FTP登录失败';
+$lang['ftp_flexshare_port'] = '端口了FlexShare';
+$lang['ftp_home_share_port'] = '家庭共享端口';
+$lang['ftp_max_instances'] = '最大实例';
+$lang['ftp_maximum_instances_invalid'] = '最大实例是无效的。';
+$lang['ftp_port_reserved_for_flexshare'] = '端口预留了FlexShare。';
+$lang['ftp_server_name'] = '服务器名称';
+$lang['ftp_server_name_invalid'] = '服务器名称是无效的。';
