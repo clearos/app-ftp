51bc0dd63710fc689506d2f3d89864b084800b68
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:57:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'Дар FTP сервер таъмин идоракунии файл ва нигаҳдории истифода FTP ва FTP протоколҳои стандартӣ Амн';
+$lang['ftp_app_name'] = 'FTP Server';
+$lang['ftp_failed_logins'] = 'FTP муяссар рамзњои';
+$lang['ftp_flexshare_port'] = 'Flexshare Порт-';
+$lang['ftp_home_share_port'] = 'Home Садо Порт-';
+$lang['ftp_max_instances'] = 'Максимум њолатњо';
+$lang['ftp_maximum_instances_invalid'] = 'Максимум ҳолатҳо нодуруст аст.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Порт-маҳфуз аст барои Flexshare.';
+$lang['ftp_server_name'] = 'Номи сервер';
+$lang['ftp_server_name_invalid'] = 'Номи Server нодуруст аст.';
