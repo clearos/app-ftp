8d83cccc16af747144e220cf8e923bb2174429a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:52:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'Servo lima procuratio providet amet, et amet, et amet adipiscing utentes Latin Secure protocols';
+$lang['ftp_app_name'] = 'FTP Server';
+$lang['ftp_failed_logins'] = 'FTP robur Logins';
+$lang['ftp_flexshare_port'] = 'Port Flexshare';
+$lang['ftp_home_share_port'] = 'Share Portus Domus';
+$lang['ftp_max_instances'] = 'Maximum Instances';
+$lang['ftp_maximum_instances_invalid'] = 'Maximum instantia nulla est.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Flexshare reservat.';
+$lang['ftp_server_name'] = 'Servo Name';
+$lang['ftp_server_name_invalid'] = 'Servo nomen est invalidum.';
