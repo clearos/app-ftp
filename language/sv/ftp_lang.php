f115d1751c987197378da0762e576fd9314ccd04
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:56:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP-server ger filhantering och lagring med hjälp av vanliga FTP och FTP säkerhetsprotokoll';
+$lang['ftp_app_name'] = 'FTP-server';
+$lang['ftp_failed_logins'] = 'FTP misslyckades inloggningar';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Hemmadelning Port';
+$lang['ftp_max_instances'] = 'högsta antalet instanser';
+$lang['ftp_maximum_instances_invalid'] = 'Max fall är ogiltig.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port Reserverat för Flexshare.';
+$lang['ftp_server_name'] = 'Server namn';
+$lang['ftp_server_name_invalid'] = 'Servernamn är ogiltigt.';
