dbb5b46ec12660b0e215167867aeb42dad1103b0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:53:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'whakarato te Tūmau FTP whakahaere kōnae, me te rokiroki mā te whakamahi i te FTP me FTP tikanga paerewa Haumaru';
+$lang['ftp_app_name'] = 'Tūmau FTP';
+$lang['ftp_failed_logins'] = 'I rahua FTP takiuru';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Kāinga Share Port';
+$lang['ftp_max_instances'] = 'Tauira mōrahi';
+$lang['ftp_maximum_instances_invalid'] = 'He muhu taime mōrahi.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port waiho ana hoki Flexshare.';
+$lang['ftp_server_name'] = 'ingoa tūmau';
+$lang['ftp_server_name_invalid'] = 'He muhu te ingoa tūmau.';
