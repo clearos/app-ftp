e27546cb97669da03e6da2d0db3455018aa5c6a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:52:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'ម៉ាស៊ីនបម្រើ FTP ដែលបានផ្ដល់នូវការគ្រប់គ្រងឯកសារនិងផ្ទុកដោយប្រើ FTP និង FTP របស់ស្ដង់ដារសុវត្ថិភាពពិធីការ';
+$lang['ftp_app_name'] = 'ម៉ាស៊ីនបម្រើ FTP របស់';
+$lang['ftp_failed_logins'] = 'FTP ដែលបានបរាជ័យក្នុងការចូល';
+$lang['ftp_flexshare_port'] = 'កំពង់ផែ Flexshare';
+$lang['ftp_home_share_port'] = 'ទំព័រដើមចែករំលែកកំពង់ផែ';
+$lang['ftp_max_instances'] = 'វត្ថុអតិបរមា';
+$lang['ftp_maximum_instances_invalid'] = 'ករណីអតិបរមាមិនត្រឹមត្រូវ។';
+$lang['ftp_port_reserved_for_flexshare'] = 'រក្សាសម្រាប់ Flexshare កំពង់ផែ។';
+$lang['ftp_server_name'] = 'ឈ្មោះម៉ាស៊ីនបម្រើ';
+$lang['ftp_server_name_invalid'] = 'ឈ្មោះម៉ាស៊ីនបម្រើមិនត្រឹមត្រូវ។';
