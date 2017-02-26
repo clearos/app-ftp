06730bf6d9cc906b27dc0b2a8346efa096ab7565
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:54:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'Il Server FTP jipprovdi ġestjoni fajl u ħażna li tuża l-FTP u FTP protokolli sikuri standard';
+$lang['ftp_app_name'] = 'FTP Server';
+$lang['ftp_failed_logins'] = 'FTP rnexxielhomx logins';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Home Share Port';
+$lang['ftp_max_instances'] = 'Każijiet massimi';
+$lang['ftp_maximum_instances_invalid'] = 'każijiet massimi huwa invalidu.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port Riservat għal Flexshare.';
+$lang['ftp_server_name'] = 'server Isem';
+$lang['ftp_server_name_invalid'] = 'Isem server huwa invalidu.';
