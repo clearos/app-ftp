df4a6459d85d093316e01207628f69a333d6d685
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:55:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP Server обеспечивает управление файлами и хранения с использованием стандартного FTP и FTP протоколов Secure';
+$lang['ftp_app_name'] = 'FTP-сервер';
+$lang['ftp_failed_logins'] = 'FTP неудачных попыток входа';
+$lang['ftp_flexshare_port'] = 'FlexShare Порт';
+$lang['ftp_home_share_port'] = 'Главная Поделиться Порт';
+$lang['ftp_max_instances'] = 'Максимальное количество экземпляров';
+$lang['ftp_maximum_instances_invalid'] = 'Максимальное количество экземпляров является недействительным.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Порт Зарезервировано для FlexShare.';
+$lang['ftp_server_name'] = 'Имя сервера';
+$lang['ftp_server_name_invalid'] = 'Имя сервера недействителен.';
