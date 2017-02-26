1eb62d04e571d3a8a37f903860dea7ad4aef1c39
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:47:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'Die FTP-bediener bied lêer bestuur en bewaring met behulp van die standaard FTP en FTP Veilige protokolle';
+$lang['ftp_app_name'] = 'FTP-bediener';
+$lang['ftp_failed_logins'] = 'FTP Kon Logins';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Tuis Deel Port';
+$lang['ftp_max_instances'] = 'maksimum Gevalle';
+$lang['ftp_maximum_instances_invalid'] = 'Maksimum gevalle is ongeldig.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port voorbehou vir Flexshare.';
+$lang['ftp_server_name'] = 'bediener Naam';
+$lang['ftp_server_name_invalid'] = 'bedienernaam is ongeldig.';
