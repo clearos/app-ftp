63dfaffb04d5acee3d240a1dd77c201dfecb8cc0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:56:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP சர்வர் நிலையான, FTP மற்றும் FTP பாதுகாப்பான நெறிமுறைகளை பயன்படுத்தி கோப்பு மேலாண்மை மற்றும் சேமிப்பு வழங்குகிறது';
+$lang['ftp_app_name'] = 'FTP சர்வர்';
+$lang['ftp_failed_logins'] = 'FTP, உள் தோல்வி';
+$lang['ftp_flexshare_port'] = 'Flexshare போர்ட்';
+$lang['ftp_home_share_port'] = 'முகப்பு பகிர் போர்ட்';
+$lang['ftp_max_instances'] = 'அதிகபட்ச சந்தர்ப்பங்கள்';
+$lang['ftp_maximum_instances_invalid'] = 'அதிகபட்ச நிகழ்வுகளை தவறானது.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Flexshare போர்ட் பாதுகாக்கப்பட்டவை.';
+$lang['ftp_server_name'] = 'சர்வர் பெயர்';
+$lang['ftp_server_name_invalid'] = 'சர்வர் பெயர் தவறானது.';
