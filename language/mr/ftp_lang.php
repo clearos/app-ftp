b62095cc5664910c1193b4fcb196dabeeef25f57
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:53:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP सर्व्हर फाइल व्यवस्थापन आणि स्टोरेज मानक FTP आणि FTP, सुरक्षित प्रोटोकॉलचा वापर करून उपलब्ध';
+$lang['ftp_app_name'] = 'FTP सर्व्हर';
+$lang['ftp_failed_logins'] = 'FTP, लॉगिन अयशस्वी';
+$lang['ftp_flexshare_port'] = 'Flexshare पोर्ट';
+$lang['ftp_home_share_port'] = 'मुख्यपृष्ठ सामायिक करा पोर्ट';
+$lang['ftp_max_instances'] = 'जास्तीत जास्त उदाहरणे';
+$lang['ftp_maximum_instances_invalid'] = 'जास्तीत जास्त उदाहरणे अवैध आहे.';
+$lang['ftp_port_reserved_for_flexshare'] = 'साठी Flexshare पोर्ट राखीव.';
+$lang['ftp_server_name'] = 'सर्व्हर नाव';
+$lang['ftp_server_name_invalid'] = 'सर्व्हर नाव अवैध आहे.';
