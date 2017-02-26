4de1f2180ab4ffff0912bfe5b0726ea425636491
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:49:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'La FTP-servilo provizas dosieron mastrumado kaj stokado uzante la normo FTP kaj FTP Secure protokoloj';
+$lang['ftp_app_name'] = 'FTP Servilo';
+$lang['ftp_failed_logins'] = 'FTP Malsukcesaj logins';
+$lang['ftp_flexshare_port'] = 'Flexshare Haveno';
+$lang['ftp_home_share_port'] = 'Hejmo Konigi Haveno';
+$lang['ftp_max_instances'] = 'maksimuma petskriboj';
+$lang['ftp_maximum_instances_invalid'] = 'Maksimuma kazoj estas nevalida.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Haveno Rezervita por Flexshare.';
+$lang['ftp_server_name'] = 'servilo Nomo';
+$lang['ftp_server_name_invalid'] = 'Servilo nomo estas malvalida.';
