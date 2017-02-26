3b31b36db4df091fdfa292e0f74fb1c8673e4138
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:55:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP Server oferă de gestionare a fișierelor și de stocare folosind FTP standard și FTP protocoale securizate';
+$lang['ftp_app_name'] = 'Server FTP';
+$lang['ftp_failed_logins'] = 'FTP nu a reușit Conectări';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Casa Port Spune-le prietenilor';
+$lang['ftp_max_instances'] = 'Instanțe maxime';
+$lang['ftp_maximum_instances_invalid'] = 'instanțele maxime este nevalid.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Portul Rezervat pentru Flexshare.';
+$lang['ftp_server_name'] = 'Numele serverului';
+$lang['ftp_server_name_invalid'] = 'Numele serverului nu este valid.';
