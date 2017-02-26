bd61cf45b3e20a6afcaffe9a99cf73268eb85d5d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:49:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'FTP Server tarjoaa tiedostojen hallinta ja varastointi käyttäen FTP ja FTP Secure protokollien';
+$lang['ftp_app_name'] = 'FTP-palvelimen';
+$lang['ftp_failed_logins'] = 'FTP epäonnistui Logins';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Kotijako Port';
+$lang['ftp_max_instances'] = 'Suurin instansseja';
+$lang['ftp_maximum_instances_invalid'] = 'Suurin tapauksissa on virheellinen.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port Varattu Flexshare.';
+$lang['ftp_server_name'] = 'Palvelimen nimi';
+$lang['ftp_server_name_invalid'] = 'Palvelimen nimi on virheellinen.';
