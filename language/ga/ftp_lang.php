e4f14cdcee760e16f0a436129fe95b0c1e396e91
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:51:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'Cuireann an Freastalaí FTP bainistíochta comhad agus stórála ag baint úsáide as an caighdeán FTP agus FTP prótacail Secure';
+$lang['ftp_app_name'] = 'FTP Server';
+$lang['ftp_failed_logins'] = 'FTP Theip logins';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Baile Share Port';
+$lang['ftp_max_instances'] = 'Cásanna uasmhéid';
+$lang['ftp_maximum_instances_invalid'] = 'Is cásanna Uasmhéid neamhbhailí.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port ar Cosaint do Flexshare.';
+$lang['ftp_server_name'] = 'Server Ainm';
+$lang['ftp_server_name_invalid'] = 'ainm freastalaí neamhbhailí.';
