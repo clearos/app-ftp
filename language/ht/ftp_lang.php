4d11c29b37fbf6434a3d71cbd2c1cb31d1c82b67
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:50:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'Sèvè a ftp bay jesyon dosye ak depo lè l sèvi avèk estanda ftp ak ftp pwotokòl yo Tache';
+$lang['ftp_app_name'] = 'fTP Server';
+$lang['ftp_failed_logins'] = 'Ftp Echwe Koneksyon';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Kay pataje Port';
+$lang['ftp_max_instances'] = 'Enstans Maksimòm';
+$lang['ftp_maximum_instances_invalid'] = 'ka Maksimòm se valab.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port Rezève pou Flexshare.';
+$lang['ftp_server_name'] = 'Non sèvè';
+$lang['ftp_server_name_invalid'] = 'Non sèvè se valab.';
