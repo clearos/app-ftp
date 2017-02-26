9d0471007743bb7fc3b78f32861d9bd2223771e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:52:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'ಸ್ಟ್ಯಾಂಡರ್ಡ್ FTP ಮತ್ತು FTP ಸುರಕ್ಷಿತ ಪ್ರೋಟೋಕಾಲ್ಗಳನ್ನು ಬಳಸಿಕೊಂಡು ಕಡತ ನಿರ್ವಹಣೆ ಮತ್ತು ಸಂಗ್ರಹ FTP ಸರ್ವರ್ ಒದಗಿಸುತ್ತದೆ';
+$lang['ftp_app_name'] = 'FTP ಸರ್ವರ್';
+$lang['ftp_failed_logins'] = 'FTP ಯ ಲಾಗಿನ್ನುಗಳು ವಿಫಲವಾಗಿದೆ';
+$lang['ftp_flexshare_port'] = 'Flexshare ಪೋರ್ಟ್';
+$lang['ftp_home_share_port'] = 'ಮುಖಪುಟ ಹಂಚಿಕೊಳ್ಳಿ ಪೋರ್ಟ್';
+$lang['ftp_max_instances'] = 'ಗರಿಷ್ಠ ಪ್ರಸಂಗಗಳು';
+$lang['ftp_maximum_instances_invalid'] = 'ಗರಿಷ್ಠ ಸಂದರ್ಭಗಳಲ್ಲಿ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Flexshare ಪೋರ್ಟ್ ಕಾಯ್ದಿರಿಸಲಾಗಿದೆ.';
+$lang['ftp_server_name'] = 'ಸರ್ವರ್ ಹೆಸರು';
+$lang['ftp_server_name_invalid'] = 'ಸರ್ವರ್ ಹೆಸರು ಅಮಾನ್ಯವಾಗಿದೆ.';
