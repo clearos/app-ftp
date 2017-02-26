43059e9fc3cd0d56d514867eb9d97cdd1150138e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:50:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'Ο διακομιστής FTP παρέχει διαχείριση αρχείων και αποθήκευσης χρησιμοποιώντας το πρότυπο Secure πρωτόκολλα FTP και FTP';
+$lang['ftp_app_name'] = 'διακομιστή FTP';
+$lang['ftp_failed_logins'] = 'FTP απέτυχε Συνδέσεις';
+$lang['ftp_flexshare_port'] = 'Port Flexshare';
+$lang['ftp_home_share_port'] = 'Αρχική σελίδα Μοιραστείτε το Λιμάνι';
+$lang['ftp_max_instances'] = 'Μέγιστος αριθμός παρουσιών';
+$lang['ftp_maximum_instances_invalid'] = 'Η μέγιστη περιπτώσεις δεν είναι έγκυρο.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port Προορίζεται για Flexshare.';
+$lang['ftp_server_name'] = 'Ονομα διακομιστή';
+$lang['ftp_server_name_invalid'] = 'Όνομα διακομιστή δεν είναι έγκυρο.';
