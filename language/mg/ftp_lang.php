bc230f067d324e808ceb04f8b2ff6eeb5dbce5ae
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 09:53:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/ftp_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ftp_app_description'] = 'Ny FTP Server manome rakitra fitantanana sy ny fitehirizana mampiasa ny faneva sy ny FTP Secure FTP protocols';
+$lang['ftp_app_name'] = 'FTP Server';
+$lang['ftp_failed_logins'] = 'FTP Tsy nahomby Logins';
+$lang['ftp_flexshare_port'] = 'Flexshare Port';
+$lang['ftp_home_share_port'] = 'Home Share Port';
+$lang['ftp_max_instances'] = 'Maximum tranga';
+$lang['ftp_maximum_instances_invalid'] = 'Maximum ohatra dia tsy manan-kery.';
+$lang['ftp_port_reserved_for_flexshare'] = 'Port natokana ho Flexshare.';
+$lang['ftp_server_name'] = 'Server Name';
+$lang['ftp_server_name_invalid'] = 'Server anarany tsy manan-kery.';
